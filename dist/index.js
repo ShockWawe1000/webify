a557e8d31baa0e620737f0e68259e0c248a4dd6a
Edis Usein <edis.usein@gmail.com>
2025-02-23 17:22:49 +0100

    intro enabled

--- dist/index.js
+++ dist/index.js
@@ -341,18 +341,17 @@ __webpack_require__.r(__webpack_exports__);
 /* harmony export */   introScreen: () => (/* binding */ introScreen)
 /* harmony export */ });
 function endIntro() {
-  intro.style.display = "none";
-  // setTimeout(()=>{
-  //     let intro = document.getElementById("intro")
-  //     let logo = document.getElementById("animatedLogo")
-  //     intro.style.backgroundColor = "rgba(0,0,0,0)"
-  //     logo.style.opacity = 0;
-  //     window.scrollTo(0,0)
-  //     setTimeout(()=>{
-  //       intro.style.display = "none"
-  //       window.scrollTo(0,0)
-  //     },1000)
-  // },4300)
+  setTimeout(() => {
+    let intro = document.getElementById("intro");
+    let logo = document.getElementById("animatedLogo");
+    intro.style.backgroundColor = "rgba(0,0,0,0)";
+    logo.style.opacity = 0;
+    window.scrollTo(0, 0);
+    setTimeout(() => {
+      intro.style.display = "none";
+      window.scrollTo(0, 0);
+    }, 1000);
+  }, 4300);
 }
 function introScreen() {
   if (document.readyState !== 'loading') {
--- src/js/introScreen.js
+++ src/js/introScreen.js
@@ -1,22 +1,19 @@
 
 
 
-
-
-
 function endIntro(){
-   intro.style.display = "none"
-    // setTimeout(()=>{
-    //     let intro = document.getElementById("intro")
-    //     let logo = document.getElementById("animatedLogo")
-    //     intro.style.backgroundColor = "rgba(0,0,0,0)"
-    //     logo.style.opacity = 0;
-    //     window.scrollTo(0,0)
-    //     setTimeout(()=>{
-    //       intro.style.display = "none"
-    //       window.scrollTo(0,0)
-    //     },1000)
-    // },4300)
+
+    setTimeout(()=>{
+        let intro = document.getElementById("intro")
+        let logo = document.getElementById("animatedLogo")
+        intro.style.backgroundColor = "rgba(0,0,0,0)"
+        logo.style.opacity = 0;
+        window.scrollTo(0,0)
+        setTimeout(()=>{
+          intro.style.display = "none"
+          window.scrollTo(0,0)
+        },1000)
+    },4300)
 
 }
 
